c790f8849727db61aa70e50ec62ad0b8c95630a4
suzukimain <131413573+suzukimain@users.noreply.github.com>
2024-10-03 12:57:52 +0900

    Update device_config.py

--- src/auto_diffusers/setup/device_config.py
+++ src/auto_diffusers/setup/device_config.py
@@ -1,5 +1,5 @@
+import torch
 import jax
-import tensorflow as tf
 
 from ..utils.get_custom_logger import custom_logger
 
@@ -10,39 +10,14 @@ class device_set:
         self.logger = custom_logger()
     
     def device_type_check(self):
-        if tf.config.experimental.list_physical_devices('GPU'):
-            device_type = "cuda"
-        else:
-            try:
-                tpu = tf.distribute.cluster_resolver.TPUClusterResolver() # TPUの検出
-                tf.tpu.experimental.initialize_tpu_system(tpu)
-                device_type = "TPU"
-            except ValueError:
-                device_type = "cpu"
-        return device_type
-
-
-    def extra_device_type_check(self):
-        _device_type = jax.devices()[0].device_kind
-        if "TPU" in _device_type:
-            return "TPU"
-        elif "cpu" in _device_type:
-            return "cpu"
-        else:
+        if torch.cuda.is_available():
             return "cuda"
-
-
-    def extra_device_set(self):
-        device_type = self.device_type_check()
-        
-        if device_type == "TPU":
-            #import torch_xla.core.xla_model as xm
-            #device = xm.xla_device()
-            device = tf.distribute.cluster_resolver.TPUClusterResolver() # TPUの検出
-            tf.tpu.experimental.initialize_tpu_system(device)
         else:
-            device = device_type
-        return device
+            device_type = jax.devices()[0].device_kind
+            if "TPU" in device_type:
+                return "TPU"
+            else:
+                return "cpu"
 
     
     def count_device(self):
