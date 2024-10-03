e040d362982f2bb892c80dcff3e9a16cddd3955d
suzukimain <131413573+suzukimain@users.noreply.github.com>
2024-10-04 07:45:28 +0900

    Change run_search to model_search

--- src/auto_diffusers/__init__.py
+++ src/auto_diffusers/__init__.py
@@ -3,4 +3,4 @@ from .utils.run_download import download
 from .checker.get_checkpoint_type import checkpoint_type
 from .model_path.perform_path_search import Search_cls
 
-run_search = Search_cls()
+model_search = Search_cls()
