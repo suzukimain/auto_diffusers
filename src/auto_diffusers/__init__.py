cc67a5b071e0a86c49151ad3486af423707b7d4c
suzukimain <131413573+suzukimain@users.noreply.github.com>
2024-10-15 07:39:57 +0900

    Update __init__.py

--- src/auto_diffusers/__init__.py
+++ src/auto_diffusers/__init__.py
@@ -2,4 +2,5 @@ from .setup.base_config import Basic_config
 from .utils.run_download import download
 from .model_path.perform_path_search import Search_cls
 
+# `run_search` is obsolete. Please use `model_search` instead.
 model_search = Search_cls()
