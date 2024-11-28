5267c86341b4807ad4b5606facb4d70b0f70cf22
suzukimain <taka222e@gmail.com>
2024-11-28 21:18:16 +0900

    Add __init__.py

---
+++ src/auto_diffusers/__init__.py
@@ -0,0 +1,8 @@
+from pipeline_easy import (
+    search_huggingface,
+    search_civitai,
+    load_pipeline_from_single_file,
+    EasyPipelineForText2Image,
+    EasyPipelineForImage2Image,
+    EasyPipelineForInpainting,
+)
