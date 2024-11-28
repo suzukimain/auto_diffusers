edac00523274a46d40760e808ef732498814e968
suzukimain <131413573+suzukimain@users.noreply.github.com>
2024-11-28 00:34:30 +0000

    Organize the repository

--- src/auto_diffusers/old/__init__.py
+++ src/auto_diffusers/old/__init__.py
@@ -1,5 +1,3 @@
-from .setup.base_config import Basic_config
-from .utils.run_download import download
 from .model_path.perform_path_search import Search_cls
 
 # `run_search` is obsolete. Please use `model_search` instead.
